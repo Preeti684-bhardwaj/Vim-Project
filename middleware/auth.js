617a7a2722e56eb1426276d3b889117bc0377287
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-07-24 10:33:41 +0530

    auth file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,71 +1,78 @@
-const User = require("../modal/userModal.js")
-const jwt = require("jsonwebtoken")
-const ErrorHandler = require("../utils/errorHandler.js")
+const User = require("../modal/userModal.js");
+const jwt = require("jsonwebtoken");
+const ErrorHandler = require("../utils/errorHandler.js");
 
-const verifyJWt = async (req , res , next)=>{
-   try {
+const verifyJWt = async (req, res, next) => {
+  try {
+    let token = req.headers["authorization"];
 
-     let token = req.headers["authorization"]
- 
-     if(!token){
-         return next(
-             new ErrorHandler(
-                 "Please Login to access this resource",
-                 401
-             )
-         )
-     }
- 
-     token = token.split(" ")[1]
- 
-     if(!token || token == "null"){
-         return next(
-             new ErrorHandler(
-                 "Please Login to access this resource",
-                 401
-             )
-         )
-     }
- 
-     const decodedToken = jwt.verify(token , process.env.JWT_SECRET)
- 
-     if(!decodedToken){
-         return next(
-             new ErrorHandler(
-                 "Invalid token or Token is Expired",
-                 401
-             )
-         )
-     }
- 
-     const user = await User.findByPk(decodedToken.id,{
-        attributes:{
-            exclude: ["password"]
-        }
-     })
-
-     if(!user){
-        return next(
-            new ErrorHandler(
-                "Invalid Access Token or user not found",
-                401
-            )
-        )
-     }
-
-     req.user = user
-     next()
-
-   } catch (error) {
-       console.log("catch auth error" , error.message)
-     return next(
+    if (!token) {
+      return next(
         new ErrorHandler(
-            "Invalid Access Token",
-            error.message,
-            401
+          "Please Login to access this resource",
+          401
         )
-     )
-   }
-}
+      );
+    }
 
-module.exports = {verifyJWt}
+    token = token.split(" ")[1];
+
+    if (!token || token === "null") {
+      return next(
+        new ErrorHandler(
+          "Please Login to access this resource",
+          401
+        )
+      );
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findByPk(decodedToken.id, {
+      attributes: {
+        exclude: ["password"]
+      }
+    });
+
+    if (!user) {
+      return next(
+        new ErrorHandler(
+          "Invalid Access Token or user not found",
+          401
+        )
+      );
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("catch auth error", error.message);
+
+    if (error.name === "TokenExpiredError") {
+      return next(
+        new ErrorHandler(
+          "Token expired. Please login again.",
+          401
+        )
+      );
+    }
+
+    if (error.name === "JsonWebTokenError") {
+      return next(
+        new ErrorHandler(
+          "Invalid token. Please login again.",
+          401
+        )
+      );
+    }
+
+    return next(
+      new ErrorHandler(
+        "Invalid Access Token",
+        401
+      )
+    );
+  }
+};
+
+module.exports = { verifyJWt };
