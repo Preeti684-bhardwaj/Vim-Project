df3c58151d495d371c19e64514736d8e972a0bf8
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-07-25 18:01:34 +0530

    limit removed

--- middleware/multer.js
+++ middleware/multer.js
@@ -10,6 +10,6 @@ const storage = multer.diskStorage({
     },
 });
 
-const upload = multer({ storage: storage,limits: { fileSize: 25 * 1024 * 1024 },  });
+const upload = multer({ storage: storage });
 
 module.exports = upload
