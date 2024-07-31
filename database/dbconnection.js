7a0f7be7c7364c2a300fcbf12a4c489a56339db0
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-07-31 13:46:01 +0530

    removed alter true

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URI,{
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await sequelize.sync({alter:true});
+    const connectionInstance = await sequelize.sync();
     console.log("Database Connected Successfully !! DB Host :", connectionInstance.config.host);
   } catch (err) {
     console.log("Database Error:", err);
